f7dd75405641a2139e3398c26808013356d906b5
ceciliah <ceciliah@spotify.com>
2019-04-01 12:22:21 +0900

    Add sample swift for testing dependency setup

---
+++ SampleSwift/SampleSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSwift
+//
+//  Created by ceciliah on 3/3/31 H.
+//  Copyright © 31 Heisei Humlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
